58cca74b5732b1850f268f573117fe5d8c662985
nvrr <sureshvarma809@gmail.com>
2022-09-01 05:23:47 -0700

    comparison query operators

---
+++ index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+ .then(() => console.log('Connected to mongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err))
+
+
+  const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: {type:Date, default: Date.now},
+    isPublished: Boolean
+  })
+
+  const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+
+    
+    
+       const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: [ 'angular','frontend'],
+        isPublished: true
+       })
+    
+        const result = await course.save()
+console.log(result);    
+}
+
+async function getCourses(){
+const courses = await Course
+//    .find({author:'Mosh',isPublished: true})
+// .find({price: {$gte : 10,lte: 20}})
+.find({price: {$in:[10,15,20]}})
+   .limit(10)
+   .sort({name:1}) //ascend 1 desend -1
+   .select({name: 1 , tags: 1})    
+
+console.log(courses);
+}
+
+getCourses()
